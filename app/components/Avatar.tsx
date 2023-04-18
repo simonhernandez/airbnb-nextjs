e37c4de50f26548aa0266cae904e878cd46620e9
Simon Barboza <simonbarboza@168.0.16.172.net-uno.net>
2023-04-17 21:19:06 -0400

    feature: categories

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -3,14 +3,20 @@
 import React from 'react'
 import Image from 'next/image';
 
-const Avatar = () => {
+interface AvatarProps {
+  src?: string | null | undefined
+}
+
+const Avatar: React.FC<AvatarProps> = (
+  {src}
+) => {
   return (
     <Image
         className='rounded-full'
         height={'30'}
         width={'30'}
         alt='Avatar'
-        src={'/images/placeholder.jpeg'}
+        src={src || '/images/placeholder.jpeg'}
     />
   )
 }
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,48 +1,47 @@
-'use client';
+"use client";
 
-import React from 'react'
-import Container from '../Container'
-import Logo from './Logo';
-import Search from './Search';
-import UserMenu from './UserMenu';
-import { User } from '@prisma/client';
-import { SafeUser } from '@/app/types';
+import React from "react";
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
+import { User } from "@prisma/client";
+import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 
 interface NavbarProps {
-    currentUser?: SafeUser | null
+  currentUser?: SafeUser | null;
 }
 
-const Navbar: React.FC<NavbarProps> = ({
-    currentUser
-}) => {
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
-    <nav className='fixed w-full bg-white z-10 shadow-sm'>
-        <div 
-            className="
+    <nav className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
                 py-4
                 border-b-[1px]
             "
-        >
-            <Container>
-                <div
-                    className='
+      >
+        <Container>
+          <div
+            className="
                         flex
                         flex-row
                         items-center
                         justify-between
                         gap-3
                         md:gap-0
-                    '
-                >
-                    <Logo />
-                    <Search />
-                    <UserMenu currentUser={currentUser}/>
-
-                </div>
-            </Container>
-        </div>
+                    "
+          >
+            <Logo />
+            <Search />
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
+      </div>
+      <Categories />
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
