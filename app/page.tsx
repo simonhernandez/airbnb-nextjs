8af448d4b53ad427a7e4e8243ea05fcc3cb4e799
Simon Barboza <simonbarboza3008@gmail.com>
2023-05-09 18:28:28 -0400

    clean

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
-import ClientOnly from './components/ClientOnly'
-import Container from './components/Container'
-import EmptyState from './components/EmptyState'
-import ListingCard from './components/listings/ListingCard'
+import Image from "next/image";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-import getListings, { IListingsParams } from './actions/getListings'
-import getCurrentUser from './actions/getCurrentUser'
+import getListings, { IListingsParams } from "./actions/getListings";
+import getCurrentUser from "./actions/getCurrentUser";
 
 interface HomeProps {
   searchParams: IListingsParams;
@@ -20,25 +20,25 @@ const Home = async ({ searchParams }: HomeProps) => {
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
-    )
+    );
   }
   return (
     <ClientOnly>
       <Container>
-        <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {listings.map((listing) => {
             return (
-              <ListingCard 
+              <ListingCard
                 key={listing.id}
                 data={listing}
                 currentUser={currentUser}
               />
-            )
+            );
           })}
         </div>
       </Container>
     </ClientOnly>
-  )
-}
+  );
+};
 
 export default Home;
